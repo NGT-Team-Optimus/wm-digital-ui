9b908422ebf4cd30c5f5be1652c5e4856d55dfe1
V V <anshid-ameen.v-v@capgemini.com>
2023-09-22 16:52:22 +0530

    goals module

--- src/app/modules/goals/goals.component.ts
+++ src/app/modules/goals/goals.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalsComponent implements OnInit {
 
+  currentHeading: number = 1;
+  heading: string = 'Short Term';
+  goForward() {
+    if (this.currentHeading < 3) {
+      this.currentHeading++;
+    }
+  }
+
+  goBackward() {
+    if (this.currentHeading > 1) {
+      this.currentHeading--;
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
