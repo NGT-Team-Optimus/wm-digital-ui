7f18bd59722bf6374b2f4e4136091379dc4b11ff
Sindhu <nanreddygari.sindhu@capgemini.com>
2023-09-29 15:19:27 +0530

    code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,14 @@ import { SignUpComponent } from './modules/sign-up/sign-up.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { PortfolioComponent } from './modules/portfolio/portfolio.component';
 import { GoalsComponent } from './modules/goals/goals.component';
-
-import { AuthenticationComponent } from './authentication/authentication.component';
+import { AuthenticationComponent } from './modules/authentication/authentication.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-
-
 import { LoginHeaderComponent } from './layout/login-header/login-header.component';
-import { OtpComponent } from './otp/otp.component';
-import { SettingPasswordComponent } from './setting-password/setting-password.component';
+import { OtpComponent } from './modules/otp/otp.component';
+import { SettingPasswordComponent } from './modules/setting-password/setting-password.component';
 
 
 @NgModule({
