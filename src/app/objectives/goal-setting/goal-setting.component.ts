4ed3fe91ea96d775873e51d9ddd541a6351ed872
V V <anshid-ameen.v-v@capgemini.com>
2023-11-09 10:15:13 +0530

    goal setting changes

--- src/app/objectives/goal-setting/goal-setting.component.ts
+++ src/app/objectives/goal-setting/goal-setting.component.ts
@@ -7,69 +7,45 @@ import { GoalModel } from 'src/app/service/goal-model';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
+
 @Component({
   selector: 'app-goal-setting',
   templateUrl: './goal-setting.component.html',
   styleUrls: ['./goal-setting.component.scss']
 })
 export class GoalSettingComponent implements OnInit {
-  goalModel: GoalModel = new GoalModel();
-  selectedButtons: string[] = [];
-  selectedGoals: GoalModel[] = [];
-  trueGoals: GoalModel[] = [];
 
-  constructor(private apiService: ApiService, private fb: FormBuilder) {
-  }
-  getGoals() {
-    this.selectedGoals = [
-      { goalId: 1, goalName: "Marrige", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 2, goalName: "Childern Marrige", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 3, goalName: "Car", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 4, goalName: "Villa", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 5, goalName: "Apartment", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 6, goalName: "Buy Land", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 7, goalName: "Marrige Anniversary", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 8, goalName: "Super Bikes", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 9, goalName: "Enterpreneurship", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 10, goalName: "Parent Marrige Anniversary", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 11, goalName: "Super Cars", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 12, goalName: "Superbike", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 13, goalName: "TomorrowLand", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 14, goalName: "Higher Education", duration: 0, financialGoalValue: 0, isSelected: false },
-      { goalId: 15, goalName: "Family Foreign Tour", duration: 0, financialGoalValue: 0, isSelected: false },
-
-    ]
-  }
-  ngOnInit(): void {
-    this.apiService.getAllUsers().subscribe(
-      (data) => { this.goalModel = data }
-    );
-  }
-
-  buttonNames: string[] = [];
+  goalModel: GoalModel[] = []
+  trueGoals: any[] = [];
   maxButtons = 5;
+  constructor(private apiService: ApiService) {
+  }
 
-  onChange(selectedGoals: any) {
-    selectedGoals.forEach((goal: { goalId: any; goalName: any; duration: any; financialGoalValue: any; isSelected: any; }) => {
+  ngOnInit(): void {
+    this.getAllGoals();
+  }
+  getAllGoals() {
+    this.apiService.getAllUsers().subscribe(
+      (data) => { this.goalModel = data; }
+    );
+    console.log(this.goalModel)
+  }
+
+  onChange(goalModel: any) {
+    goalModel.forEach((goal: { goalId: any; goalName: any; duration: any; financialGoalValue: any; isSelected: any; }) => {
       if (goal.isSelected === true) {
         this.trueGoals.push(goal)
       }
     });
     console.log(this.trueGoals)
   }
-
-  onCheckboxChange(selectedGoals: GoalModel[]) {
-    for (const goal of selectedGoals) {
+  onCheckboxChange(goalModel: GoalModel[]) {
+    for (const goal of goalModel) {
       goal.isSelected = !goal.isSelected;
     }
   }
   countSelectedGoals(): number {
-    return this.selectedGoals.filter((goal) => goal.isSelected).length;
+    return this.goalModel.filter((goal) => goal.isSelected).length;
   }
 }
 
-class goalModel {
-  goalId!: number;
-  goalName!: string;
-  isSelected!: boolean;
-}
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -12,11 +12,8 @@ export class ApiService implements OnInit {
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
-  getAllUsers(): Observable<any> {
-    return this.httpClient.get("http://localhost:8080/goals/get");
-  }
-  goalSet(goalModel: GoalModel): Observable<any> {
-    return this.httpClient.post("http://localhost:8080/addGoals", goalModel);
+  getAllUsers(): Observable<GoalModel[]> {
+    return this.httpClient.get<GoalModel[]>('http://localhost:8080/goals/get');
   }
 
 
