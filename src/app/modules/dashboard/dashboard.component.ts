dedd0d7743e5b7fbdd6eef075814dc822b181d99
prasanna laxmi <bura.laxmi@capgemini.com>
2023-12-03 11:06:29 +0530

    dashboard update

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -10,20 +10,58 @@ export class DashboardComponent implements OnInit {
 
   //name : string = 'Hola, Sam';
   name: string | undefined;
-  numberOfGoals: string | undefined ;
+  numberOfGoals!: number;
+  title!: string;
+  holding : string = 'holding amout: $ 0.0'
+   percentage!: number;
+  goals: any[] = [];
 
 response: any;
   constructor(private ApiService: ApiService) { }
 
   ngOnInit(): void {
+
     this.ApiService.getUsername().subscribe(
       (data: any) => {
         this.name = data.user.username;
-        console.log(data);
         this.numberOfGoals = data.numberOfGoals;
       }
     );
-    
+      this.ApiService.getUsername().subscribe(
+        (data: any) => {
+         const  goal = data.goals;
+         this.goals = goal;
+          console.log(this.goals);
+          this.numberOfGoals = data.numberOfGoals;
+          const number1 : number= this.numberOfGoals;
+          let sumOfFinancialValue =0;
+          for (let i=0; i< number1; i++ ){
+            sumOfFinancialValue= this.goals[i].financialGoalValue+sumOfFinancialValue;
+          }
+          console.log("total sum "+sumOfFinancialValue);
+          let totalAmount = 0;
+          for ( let i=0; i<5; i++){
+            totalAmount = this.goals[i].totalAmount + totalAmount;
+          }
+          console.log("total amount "+totalAmount);
+          let percent: number = (totalAmount/sumOfFinancialValue)*100;
+          console.log("total percentage "+percent);
+          let floatPercnt: number = parseFloat(percent.toFixed(2));
+          this.percentage = floatPercnt;
+          const holding_amount = "Holding amount: $"+totalAmount;
+          this.holding = holding_amount;
+          const title1 = " "+this.percentage;
+          this.title = title1;
+        }
+      );
+      
+     
+      
+
   }
+  
+  
         
 }
+
+
